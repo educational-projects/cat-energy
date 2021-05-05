5645a42ef8112055aa02d44813ba888dcb33083d
ваше имя <dimarik-tixonov@yandex.ru>
2021-05-05 20:02:47 +0300

    доработка главной станицы мобильной версии

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const svgSprite = require("gulp-svg-sprite");
+const svgmin = require("gulp-svgmin");
+const	cheerio = require("gulp-cheerio");
+const	replace = require("gulp-replace");
 
 // Styles
 
@@ -50,3 +54,41 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// svg-sprite
+
+gulp.task('svgSpriteBuild', function () {
+  return gulp.src(assetsDir + 'd/project/1697997-cat-energy-22/source/img/svg/*.svg')
+  // minify svg
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    // remove all fill, style and stroke declarations in out shapes
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {xmlMode: true}
+    }))
+    // cheerio plugin create unnecessary string '&gt;', so replace it.
+    .pipe(replace('&gt;', '>'))
+    // build svg sprite
+    .pipe(svgSprite({
+      mode: {
+        symbol: {
+          sprite: "../sprite.svg",
+          render: {
+            scss: {
+              dest:'../../../sass/_sprite.scss',
+              template: assetsDir + "sass/templates/_sprite_template.scss"
+            }
+          }
+        }
+      }
+    }))
+    .pipe(gulp.dest(assetsDir + 'i/sprite/'));
+});
