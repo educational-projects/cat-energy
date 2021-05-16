da3423cc31034afe338d2b5e1de1a28696c5e8d0
ваше имя <dimarik-tixonov@yandex.ru>
2021-05-16 23:48:37 +0300

    вставка всг спрайта

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,22 @@ const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
 const svgSprite = require("gulp-svg-sprite");
 const svgmin = require("gulp-svgmin");
-const	cheerio = require("gulp-cheerio");
-const	replace = require("gulp-replace");
+const cheerio = require("gulp-cheerio");
+const replace = require("gulp-replace");
 
 // Styles
 
 const styles = () => {
-  return gulp.src("source/sass/style.scss")
+  return gulp
+    .src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
 
@@ -32,15 +31,15 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "source",
     },
-    browser: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+    browser: "C:/Program Files/Google/Chrome/Application/chrome.exe",
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
 
@@ -49,46 +48,36 @@ exports.server = server;
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
-}
+};
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+exports.default = gulp.series(styles, server, watcher);
 
 // svg-sprite
 
-gulp.task('svgSpriteBuild', function () {
-  return gulp.src(assetsDir + 'd/project/1697997-cat-energy-22/source/img/svg/*.svg')
-  // minify svg
-    .pipe(svgmin({
-      js2svg: {
-        pretty: true
-      }
-    }))
-    // remove all fill, style and stroke declarations in out shapes
-    .pipe(cheerio({
-      run: function ($) {
-        $('[fill]').removeAttr('fill');
-        $('[stroke]').removeAttr('stroke');
-        $('[style]').removeAttr('style');
-      },
-      parserOptions: {xmlMode: true}
-    }))
-    // cheerio plugin create unnecessary string '&gt;', so replace it.
-    .pipe(replace('&gt;', '>'))
-    // build svg sprite
-    .pipe(svgSprite({
-      mode: {
-        symbol: {
-          sprite: "../sprite.svg",
-          render: {
-            scss: {
-              dest:'../../../sass/_sprite.scss',
-              template: assetsDir + "sass/templates/_sprite_template.scss"
-            }
-          }
-        }
-      }
-    }))
-    .pipe(gulp.dest(assetsDir + 'i/sprite/'));
+// sprite
+const sprite = "sprite",
+  spriteFiles = "source/img/icons/*.svg",
+  spritePublic = "source/img/sprite";
+
+gulp.task(sprite, function () {
+  return gulp
+    .src(spriteFiles)
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            prefix: ".icon-%s",
+            dimensions: "%s",
+            sprite: "../sprite.svg",
+            render: {
+              scss: {
+                dest: "../sprite.scss",
+              },
+            },
+          },
+        },
+      })
+    )
+    .pipe(gulp.dest(`source/img/sprite/`))
+    .pipe(gulp.dest(spritePublic));
 });
