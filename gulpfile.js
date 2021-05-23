002d9a565aed105ffc7ebebefbaa6574fc8721aa
ваше имя <dimarik-tixonov@yandex.ru>
2021-05-23 20:47:19 +0300

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const terser = require("gulp-terser");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const del = require("del");
 const sync = require("browser-sync").create();
 const htmlmin = require("gulp-htmlmin")
 const svgSprite = require("gulp-svg-sprite");
@@ -20,9 +26,12 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()]))
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 };
 
@@ -38,35 +47,51 @@ const html = () => {
 
 exports.html = html;
 
-// Server
+// Scripts
 
-const server = (done) => {
-  sync.init({
-    server: {
-      baseDir: "source",
-    },
-    browser: "C:/Program Files/Google/Chrome/Application/chrome.exe",
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
-};
+const scripts = () => {
+  return gulp.src("source/js/script.js", {"allowEmpty": true})
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
 
-exports.server = server;
+exports.scripts = scripts;
 
-// Watcher
+// Images
 
-const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
-};
+const optimizeImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
 
-exports.default = gulp.series(styles, server, watcher);
+exports.images = optimizeImages;
 
-// svg-sprite
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"))
+}
 
-// sprite
+exports.images = copyImages;
+
+// Webp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
+
+// Sprite
+
 const sprite = "sprite",
   spriteFiles = "source/img/icons/*.svg",
   spritePublic = "source/img/sprite";
@@ -90,6 +115,97 @@ gulp.task(sprite, function () {
         },
       })
     )
-    .pipe(gulp.dest(`source/img/sprite/`))
+    .pipe(gulp.dest(`build/img/sprite/`))
     .pipe(gulp.dest(spritePublic));
 });
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.svg",
+    "!source/img/icons/*.svg",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: "build",
+    },
+    browser: "C:/Program Files/Google/Chrome/Application/chrome.exe",
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+};
+
+exports.server = server;
+
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch('source/*.html', gulp.series(html, reload));
+};
+
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
+);
+
+exports.build = build;
+
+// Default
+
+exports.default = gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
